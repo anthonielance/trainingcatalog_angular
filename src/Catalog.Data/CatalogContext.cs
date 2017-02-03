1804b6f61d9079387d859b8a8a98eadecf8f3d7a
anthonielance <alance33@gmail.com>
2017-02-03 14:51:59 -0500

    Add entity framework core

---
+++ src/Catalog.Data/CatalogContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using USF.TrainingCatalog.Data.Entities;
+
+namespace USF.TrainingCatalog.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Material> Materials { get; set; }
+
+        public DbSet<CatalogUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Session>()
+                .Property(s => s.RowVersion)
+                .ValueGeneratedOnAddOrUpdate()
+                .IsConcurrencyToken();
+
+            builder.Entity<Trainer>()
+                .Property(t => t.RowVersion)
+                .ValueGeneratedOnAddOrUpdate()
+                .IsConcurrencyToken();
+
+            builder.Entity<Attendee>()
+                .Property(a => a.RowVersion)
+                .ValueGeneratedOnAddOrUpdate()
+                .IsConcurrencyToken();
+
+            builder.Entity<Material>()
+                .Property(m => m.RowVersion)
+                .ValueGeneratedOnAddOrUpdate()
+                .IsConcurrencyToken();
+
+            builder.Entity<CatalogUser>()
+                .Property(u => u.RowVersion)
+                .ValueGeneratedOnAddOrUpdate()
+                .IsConcurrencyToken();
+        }
+    }
+}
